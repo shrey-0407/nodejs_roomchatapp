2433506faef73865d6df0793a5b52b66b937ac15
shrey <shreyash966977@gmail.com>
2023-11-28 11:36:30 +0530

    commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var router = express.Router();
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const port = process.env.PORT || 8080;
+http.listen(port, 'localhost', () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/nodechatapp')
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.plugin(plm)
+
+module.exports = mongoose.model('User', userSchema);
+
